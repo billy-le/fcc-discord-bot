797ab43f42a3c2f1abdc262c7d783b5b11363ce9
Billy Le <lebilly87@gmail.com>
2018-12-19 18:55:46 -0800

    added server to keep heroku from crashing

--- dist/app.d.ts
+++ dist/app.d.ts
@@ -1,6 +1,7 @@
 declare const fs: any;
 declare const path: any;
+declare const http: any;
 declare const prefix: any;
 declare const Discord: any;
-declare const client: any;
-declare const commandFiles: any;
+declare const PORT: string | number;
+declare const server: any;
--- src/app.ts
+++ src/app.ts
@@ -1,40 +1,51 @@
 require("dotenv").config();
 const fs = require("fs");
 const path = require("path");
+const http = require("http");
 const { prefix } = require("./config");
-
 const Discord = require("discord.js");
 
-const client = new Discord.Client();
-client.commands = new Discord.Collection();
+const PORT: string | number = process.env.PORT || 3000;
 
-const commandFiles = fs
-  .readdirSync(__dirname + "/commands")
-  .filter((file: any) => file.endsWith(".js"));
+const server = http.createServer((req: any, res: any) => {
+  req;
+  const client = new Discord.Client();
+  client.commands = new Discord.Collection();
 
-for (const file of commandFiles) {
-  const command = require(`./commands/${file}`);
-  client.commands.set(command.name, command);
-}
+  const commandFiles = fs
+    .readdirSync(__dirname + "/commands")
+    .filter((file: any) => file.endsWith(".js"));
 
-client.on("message", (message: any) => {
-  if (!message.content.startsWith(prefix) || message.author.bot) return;
-
-  const args = message.content.slice(prefix.length).split(/ +/);
-  const commandName = args.shift().toLowerCase();
-
-  if (!client.commands.has(commandName)) {
-    return;
+  for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+    client.commands.set(command.name, command);
   }
 
-  const command = client.commands.get(commandName);
+  client.on("message", (message: any) => {
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
 
-  try {
-    command.execute(message, args);
-  } catch (error) {
-    console.error(error);
-    message.reply("there was an error trying to execute that command!");
-  }
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const commandName = args.shift().toLowerCase();
+
+    if (!client.commands.has(commandName)) {
+      return;
+    }
+
+    const command = client.commands.get(commandName);
+
+    try {
+      command.execute(message, args);
+    } catch (error) {
+      console.error(error);
+      message.reply("there was an error trying to execute that command!");
+    }
+  });
+
+  client.login(process.env.DISCORD_TOKEN);
+
+  return res.end("<h1>App is live</h1>");
 });
 
-client.login(process.env.DISCORD_TOKEN);
+server.listen(PORT, () =>
+  console.log(`The server is running on http://localhost:${PORT}`)
+);
