b5fb96b509e708e7c8a668b005537ba1e1b6eb53
Richard Prasad <richarddprasad@gmail.com>
2018-12-25 12:56:22 -0800

    Excluding dist directory

--- dist/commands/test.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-module.exports = {
-    name: "test",
-    description: "testing",
-    execute: function (message, args) {
-        message.channel.send("Subscribing!");
-    }
-};
-//# sourceMappingURL=test.js.map
